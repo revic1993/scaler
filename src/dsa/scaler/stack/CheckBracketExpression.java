689147de7cb7a8bfae9a1b0523da30492b764527
Rujul Solanki <rujul.reg@gmail.com>
2021-12-21 18:26:31 +0530

    Stack

--- src/dsa/scaler/stack/CheckBracketExpression.java
+++ src/dsa/scaler/stack/CheckBracketExpression.java
@@ -1,38 +1,50 @@
 package dsa.scaler.stack;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Stack;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class CheckBracketExpression {
     public static void main(String[] args) {
         CheckBracketExpression cbe = new CheckBracketExpression();
-//        System.out.println(cbe.solve("-(-(-(-a+b)-d+c)-q)","a-b-d+c+q"));
-//        System.out.println(cbe.solve("-(a+b+c)","-a-b-c"));
-//        System.out.println(cbe.solve("a-b-(c-d)","a-b-c-d"));
+        System.out.println(cbe.solve("-(-(-(-a+b)-d+c)-q)","a-b-d+c+q"));
+        System.out.println(cbe.solve("-(a+b+c)","-a-b-c"));
+        System.out.println(cbe.solve("a-b-(c-d)","a-b-c-d"));
         System.out.println(cbe.solve("-(a-(-z-(b-(c+t)-x)+l)-q)","-a+l-b-(z-(c+t)-x-q)"));
-        //A : c+l+q+t+x+z-a-b
-        //B : c+l+q+t+x-a-b-z
-        //
-//        System.out.println(cbe.solve("-(-z+a)","z-a"));
+        System.out.println(cbe.solve("-(-z+a)","z-a"));
+        System.out.println(cbe.solve("-(a+((b-c)-(d+e)))","-(a+b-c-d-e)"));
 
     }
+
+    HashSet<Character> signs = new HashSet<>();
+    HashSet<Character> braces = new HashSet<>();
+    HashSet<Character> alphabets = new HashSet<>();
     public int solve(String A, String B) {
-        System.out.println("A : "+ flattenString(A));
-        System.out.println("B : "+flattenString(B));
-       return flattenString(A).equals(flattenString(B)) ? 1 : 0;
-    }
-
-    String flattenString(String data){
-        HashSet<Character> signs = new HashSet<>();
         signs.add('+');
         signs.add('-');
-        HashSet<Character> braces = new HashSet<>();
+
         braces.add('(');
         braces.add(')');
-        //        Stack<Character> expStack = new Stack<>();
+
+        for(int i='a';i<='z';i++){
+            alphabets.add((char) i);
+        }
+        char[] aFlattened = flattenString(A);
+        char[] bFlattened = flattenString(B);
+//        System.out.println(aFlattened);
+//        System.out.println(bFlattened);
+        for(int i=0;i<26;i++){
+            if(!signs.contains(aFlattened[i]) && !signs.contains(bFlattened[i])){
+                continue;
+            }
+
+            if(aFlattened[i] != bFlattened[i]){
+                return 0;
+            }
+        }
+        return 1;
+    }
+
+    char[] flattenString(String data){
         char[] scAlpha = new char[26];
         for(int i=0;i<data.length();i++){
             if(signs.contains(data.charAt(i)) || braces.contains(data.charAt(i))){
@@ -48,37 +60,39 @@ public class CheckBracketExpression {
         Stack<Boolean> negStack = new Stack<>();
         Stack<Character> expStack = new Stack<>();
         for(char datum : data.toCharArray()){
-            
-        }
+            if(datum != '('){
+                if(alphabets.contains(datum) && !negStack.isEmpty()){
+                    if(negStack.peek()){
+                        char sign = !expStack.isEmpty() && expStack.peek()=='-' ? '+' : '-';
+                        scAlpha[datum-'a'] = sign;
+                    }
+                }
+                if(datum==')' && !  negStack.isEmpty()){
+                    negStack.pop();
+                }
+                expStack.push(datum);
+                continue;
+            }
 
-//        if(datum == ')' && !negStack.isEmpty()){
-//            negStack.pop();
-//            continue;
-//        }
-//
-//        if(datum == '('){
-//            if(!expStack.isEmpty() && expStack.peek() == '-'){
-//                if(negStack.isEmpty())
-//                    negStack.push(true);
-//                else
-//                    negStack.push(!negStack.peek());
-//            }else if(expStack.isEmpty() || expStack.peek() != '-'){
-//                negStack.push(false);
-//            }
-//            continue;
-//        }
-//        expStack.push(datum);
-//
-//
-//        char[] charArr = new char[expStack.size()];
-//        int i = expStack.size()-1;
-//        while(expStack.size()!=0){
-//            charArr[i--] = expStack.pop();
-//        }
-//
-//        return new String(charArr);
-        return "";
+            if(!expStack.isEmpty() && expStack.peek() == '-'){
+                if(negStack.isEmpty()){
+                    negStack.push(true);
+                }else{
+                    negStack.push(!negStack.peek());
+                }
+            }else{
+                if(negStack.isEmpty()){
+                    negStack.push(false);
+                }else{
+                    negStack.push(negStack.peek());
+                }
+            }
+            expStack.push(datum);
+        }
+        return scAlpha;
     }
+}
+
 
        /* Stack<Character> charStack = new Stack<>();
         HashSet<Character> signs = new HashSet<>();
@@ -150,4 +164,30 @@ public class CheckBracketExpression {
         }
 
         return new String(charArr);*/
-}
+
+//        if(datum == ')' && !negStack.isEmpty()){
+//            negStack.pop();
+//            continue;
+//        }
+//
+//        if(datum == '('){
+//            if(!expStack.isEmpty() && expStack.peek() == '-'){
+//                if(negStack.isEmpty())
+//                    negStack.push(true);
+//                else
+//                    negStack.push(!negStack.peek());
+//            }else if(expStack.isEmpty() || expStack.peek() != '-'){
+//                negStack.push(false);
+//            }
+//            continue;
+//        }
+//        expStack.push(datum);
+//
+//
+//        char[] charArr = new char[expStack.size()];
+//        int i = expStack.size()-1;
+//        while(expStack.size()!=0){
+//            charArr[i--] = expStack.pop();
+//        }
+//
+//        return new String(charArr);
--- src/dsa/scaler/stack/MaxFreqStack.java
+++ src/dsa/scaler/stack/MaxFreqStack.java
@@ -2,82 +2,74 @@ package dsa.scaler.stack;
 
 import dsa.utils.Utils;
 
+import java.beans.PropertyEditorSupport;
 import java.util.*;
 
 public class MaxFreqStack {
     public static void main(String[] args) {
         ArrayList<ArrayList<Integer>> data = new ArrayList<>();
-        data.add(new ArrayList<>(List.of(1,5)));
-        data.add(new ArrayList<>(List.of(1,7)));
-        data.add(new ArrayList<>(List.of(1,5)));
-        data.add(new ArrayList<>(List.of(1,7)));
-        data.add(new ArrayList<>(List.of(1,4)));
-        data.add(new ArrayList<>(List.of(1,5)));
-        data.add(new ArrayList<>(List.of(2,0)));
-        data.add(new ArrayList<>(List.of(2,0)));
-        data.add(new ArrayList<>(List.of(2,0)));
-        data.add(new ArrayList<>(List.of(2,0)));
+        data.add(new ArrayList<>(List.of(1, 5)));
+        data.add(new ArrayList<>(List.of(1, 7)));
+        data.add(new ArrayList<>(List.of(1, 5)));
+        data.add(new ArrayList<>(List.of(1, 7)));
+        data.add(new ArrayList<>(List.of(1, 4)));
+        data.add(new ArrayList<>(List.of(1, 5)));
+        data.add(new ArrayList<>(List.of(2, 0)));
+        data.add(new ArrayList<>(List.of(2, 0)));
+        data.add(new ArrayList<>(List.of(2, 0)));
+        data.add(new ArrayList<>(List.of(2, 0)));
         MaxFreqStack maxFreqStack = new MaxFreqStack();
         Utils.printSingleArr(maxFreqStack.solve(data));
     }
+
     int highestFreq = Integer.MIN_VALUE;
-    int currentHighest = Integer.MIN_VALUE;
+
     public ArrayList<Integer> solve(ArrayList<ArrayList<Integer>> A) {
         ArrayList<Integer> result = new ArrayList<>();
-        HashMap<Integer,Integer> freqMap = new HashMap<>();
-        HashMap<Integer,HashSet<Integer>> reverseMap = new HashMap<>();
-//        Stack<Integer> numStack = new Stack<>();
-        for(ArrayList<Integer> data : A){
-            if(data.get(0) == 1){
-//                numStack.push(data.get(1));
-                updateFreq(freqMap,reverseMap,data.get(1), true);
-                result.add(-1);
-            }else{
-                result.add(currentHighest);
-                updateFreq(freqMap,reverseMap,currentHighest,false);
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+        HashMap<Integer, Stack<Integer>> reverseMap = new HashMap<>();
+        int i = 0;
+        for (ArrayList<Integer> data : A) {
+            boolean isPush = data.get(0) == 1;
+            if (isPush) {
+                result.add(push(data.get(1), freqMap, reverseMap));
+            } else {
+                result.add(pop(freqMap, reverseMap));
             }
         }
 
         return result;
     }
 
-    public void updateFreq(HashMap<Integer,Integer> freqMap,HashMap<Integer,HashSet<Integer>> reverseMap,int a,boolean push){
-        if(!freqMap.containsKey(a)){
-            freqMap.put(a,0);
+    private int pop(HashMap<Integer, Integer> freqMap, HashMap<Integer, Stack<Integer>> reverseMap) {
+        Stack<Integer> freqNumStack = reverseMap.get(this.highestFreq);
+        int currentHighestFreqNum = freqNumStack.pop();
+        if(freqNumStack.isEmpty()){
+            reverseMap.remove(this.highestFreq);
+            this.highestFreq--;
+        }else{
+            reverseMap.put(this.highestFreq,freqNumStack);
+        }
+        int currHighestFreq = freqMap.get(currentHighestFreqNum);
+        if(currHighestFreq-1 == 0){
+            freqMap.remove(currentHighestFreqNum);
+        }else{
+            freqMap.put(currentHighestFreqNum,currHighestFreq-1);
+        }
+        return currentHighestFreqNum;
+    }
+
+    public int push(int a, HashMap<Integer, Integer> freqMap, HashMap<Integer, Stack<Integer>> reverseMap) {
+        if (!freqMap.containsKey(a)) {
+            freqMap.put(a, 0);
         }
 
         int currentFreq = freqMap.get(a);
-        int updatedFreq;
-        if(push){
-            updatedFreq = currentFreq+1;
-        }else{
-            updatedFreq = currentFreq-1;
-        }
-        freqMap.put(a,updatedFreq);
-        updateReverseFreq(reverseMap,currentFreq,updatedFreq,a);
-    }
-
-    public void updateReverseFreq(HashMap<Integer,HashSet<Integer>> reverseFreqMap,int removeFreq, int addFreq,int a){
-        if(reverseFreqMap.containsKey(removeFreq)){
-            HashSet<Integer> numSet = reverseFreqMap.get(removeFreq);
-            numSet.remove(a);
-            if(numSet.isEmpty()){
-                reverseFreqMap.remove(removeFreq);
-            }else{
-                reverseFreqMap.put(removeFreq,numSet);
-            }
-        }
-
-        if(!reverseFreqMap.containsKey(addFreq)){
-            HashSet<Integer> numSet = new HashSet<>();
-            numSet.add(a);
-            reverseFreqMap.put(addFreq,numSet);
-        }else{
-            HashSet<Integer> currentSet = reverseFreqMap.get(addFreq);
-            currentSet.add(a);
-            reverseFreqMap.put(addFreq,currentSet);
-        }
-        highestFreq = reverseFreqMap.keySet().stream().max(Integer::compare).get();
-        currentHighest = reverseFreqMap.get(highestFreq).iterator().next();
+        freqMap.put(a, ++currentFreq);
+        Stack<Integer> freqNumStack = !reverseMap.containsKey(currentFreq) ? new Stack<>() : reverseMap.get(currentFreq);
+        freqNumStack.push(a);
+        reverseMap.put(currentFreq, freqNumStack);
+        this.highestFreq = Math.max(this.highestFreq, currentFreq);
+        return -1;
     }
 }

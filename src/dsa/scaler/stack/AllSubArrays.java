8274de010f7e0366072aa8238e6439258e09a379
Rujul Solanki <rujul.reg@gmail.com>
2021-12-21 10:36:17 +0530

    Stack

---
+++ src/dsa/scaler/stack/AllSubArrays.java
@@ -0,0 +1,17 @@
+package dsa.scaler.stack;
+
+import java.util.HashSet;
+import java.util.Stack;
+
+public class AllSubArrays {
+    public static void main(String[] args) {
+        AllSubArrays asa = new AllSubArrays();
+
+        System.out.println(asa.solve(new int[]{2,1,7,4,3}));
+    }
+
+    public int solve(int[] A) {
+        // 2 1 7 
+      return -1;
+    }
+}
--- src/dsa/scaler/stack/CheckBracketExpression.java
+++ src/dsa/scaler/stack/CheckBracketExpression.java
@@ -1,20 +1,86 @@
 package dsa.scaler.stack;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class CheckBracketExpression {
     public static void main(String[] args) {
         CheckBracketExpression cbe = new CheckBracketExpression();
-        System.out.println(cbe.solve("-(-(-(-a+b)-d+c)-q)","a-b-d+c+q"));
+//        System.out.println(cbe.solve("-(-(-(-a+b)-d+c)-q)","a-b-d+c+q"));
+//        System.out.println(cbe.solve("-(a+b+c)","-a-b-c"));
+//        System.out.println(cbe.solve("a-b-(c-d)","a-b-c-d"));
+        System.out.println(cbe.solve("-(a-(-z-(b-(c+t)-x)+l)-q)","-a+l-b-(z-(c+t)-x-q)"));
+        //A : c+l+q+t+x+z-a-b
+        //B : c+l+q+t+x-a-b-z
+        //
+//        System.out.println(cbe.solve("-(-z+a)","z-a"));
+
     }
     public int solve(String A, String B) {
-        System.out.println( flattenString(A));
+        System.out.println("A : "+ flattenString(A));
+        System.out.println("B : "+flattenString(B));
        return flattenString(A).equals(flattenString(B)) ? 1 : 0;
     }
 
     String flattenString(String data){
-        Stack<Character> charStack = new Stack<>();
+        HashSet<Character> signs = new HashSet<>();
+        signs.add('+');
+        signs.add('-');
+        HashSet<Character> braces = new HashSet<>();
+        braces.add('(');
+        braces.add(')');
+        //        Stack<Character> expStack = new Stack<>();
+        char[] scAlpha = new char[26];
+        for(int i=0;i<data.length();i++){
+            if(signs.contains(data.charAt(i)) || braces.contains(data.charAt(i))){
+                continue;
+            }
+            char sign = '+';
+            if(i!=0 && data.charAt(i-1)=='-'){
+                sign='-';
+            }
+            scAlpha[data.charAt(i)-'a'] = sign;
+        }
+
+        Stack<Boolean> negStack = new Stack<>();
+        Stack<Character> expStack = new Stack<>();
+        for(char datum : data.toCharArray()){
+            
+        }
+
+//        if(datum == ')' && !negStack.isEmpty()){
+//            negStack.pop();
+//            continue;
+//        }
+//
+//        if(datum == '('){
+//            if(!expStack.isEmpty() && expStack.peek() == '-'){
+//                if(negStack.isEmpty())
+//                    negStack.push(true);
+//                else
+//                    negStack.push(!negStack.peek());
+//            }else if(expStack.isEmpty() || expStack.peek() != '-'){
+//                negStack.push(false);
+//            }
+//            continue;
+//        }
+//        expStack.push(datum);
+//
+//
+//        char[] charArr = new char[expStack.size()];
+//        int i = expStack.size()-1;
+//        while(expStack.size()!=0){
+//            charArr[i--] = expStack.pop();
+//        }
+//
+//        return new String(charArr);
+        return "";
+    }
+
+       /* Stack<Character> charStack = new Stack<>();
         HashSet<Character> signs = new HashSet<>();
         signs.add('+');
         signs.add('-');
@@ -49,6 +115,39 @@ public class CheckBracketExpression {
         while(charStack.size()!=0){
             charArr[i--] = charStack.pop();
         }
-        return new String(charArr);
-    }
+        return new String(charArr);*/
+        /*
+        HashSet<Character> signs = new HashSet<>();
+        signs.add('+');
+        signs.add('-');
+        Stack<Character> expStack = new Stack<>();
+        Stack<Boolean> negStack = new Stack<>();
+        for(char datum : data.toCharArray()){
+            if(datum == ')' && !negStack.isEmpty()){
+                negStack.pop();
+                continue;
+            }
+
+            if(datum == '('){
+                if(!expStack.isEmpty() && expStack.peek() == '-'){
+                    if(negStack.isEmpty())
+                        negStack.push(true);
+                    else
+                        negStack.push(!negStack.peek());
+                }else if(expStack.isEmpty() || expStack.peek() != '-'){
+                    negStack.push(false);
+                }
+                continue;
+            }
+
+            expStack.push(datum);
+        }
+
+        char[] charArr = new char[expStack.size()];
+        int i = expStack.size()-1;
+        while(expStack.size()!=0){
+            charArr[i--] = expStack.pop();
+        }
+
+        return new String(charArr);*/
 }
